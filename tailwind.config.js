3760967e6771cfe9e1682af1f536209a13e89ff1
hyunjung <poggo201103@gmail.com>
2025-08-12 03:37:43 +0900

    테일윈드 디자인

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // 리액트 컴포넌트 파일 경로
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
